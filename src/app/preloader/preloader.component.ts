44d4502a446fe8fd6a5b952067b04571d4154737
Maryam Bril <bmaryam@ept.sn>
2024-11-18 13:40:42 +0100

    second push

--- src/app/preloader/preloader.component.ts
+++ src/app/preloader/preloader.component.ts
@@ -1,11 +1,10 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-preloader',
   templateUrl: './preloader.component.html',
-  styleUrls: ['./preloader.component.css'] // Note: Corrected `styleUrl` to `styleUrls`
+  styleUrl: './preloader.component.css'
 })
 export class PreloaderComponent {
- 
+
 }
